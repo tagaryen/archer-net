c044ab42b746756ed5217ebdc7ad2f18473617a0
xuyi <xu_yi@cestc.cn>
2025-07-11 15:47:24 +0800

    update test examples

--- src/test/java/com/archer/net/test/HttpTest.java
+++ src/test/java/com/archer/net/test/HttpTest.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import com.archer.net.http.client.NativeRequest;
 import com.archer.net.http.client.NativeResponse;
+import com.archer.net.ssl.SslContext;
 
 public class HttpTest {
 	
@@ -28,12 +29,13 @@ public class HttpTest {
 	}
 	
 	public static void sslTest() {
+		SslContext ctx = new SslContext(true, false);
 		NativeRequest.Options opts = new NativeRequest.Options();
 		HashMap<String, String> headers = new HashMap<>();
 		headers.put("Token","OKCjKBw6I8ONFj2ZCrjNQJQKrsjQRc3egsVdPfMbLbUsPK7dmbh5B/OXZdyKOLPIUQwen7So1WIPooGwnRVeSYI/WUhVmkq2lWC4BXhx5UJKhnD2Uf22K//AUqYrbvGScgI08kjgnAq0fIvGnNqhL/JpSvjn2YhfB82SHIaYBJStpUmtVFkj+JHPOzJGJvaFlha/oel5p/wgww0rOxUOgq4EZOiIirh+YdsVCVOSSQUNHiCMLY2ZYH5eZEsbPyMtCCQnHc6R7vVs9lY46pcFFDBcd/VTLsWpt0WVmYxY3Iv1GDda7fdFNwEqxIP7vq1tdR8HWJR7BfGOHaByuccjBA==");
 		headers.put("Workspace", "2025061300010400001239");
 		headers.put("Content-Type", "application/json");
-		opts.headers(headers).verifyCert(false);
+		opts.headers(headers).sslcontext(ctx);
 		String body = "{\"content\":{\"workspaceId\":\"2025061300010400001239\"},\"method\":\"gaia.openapi.mine.workspace.getOne\"}";
 		
 		NativeResponse res = NativeRequest.post("https://gaiac-104.base.trustbe.cn/gaia/v1/janus/invoke/v1", body.getBytes(StandardCharsets.UTF_8), opts);
